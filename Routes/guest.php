71e47ce38bfcb5415865027e519b8ed61e16b2bd
aabc19990602 <31418304+aabc19990602@users.noreply.github.com>
2022-05-20 17:59:21 +0530

    Signed-off-by: aabc19990602 <31418304+aabc19990602@users.noreply.github.com>

--- Routes/guest.php
+++ Routes/guest.php
@@ -8,8 +8,17 @@ use Illuminate\Support\Facades\Route;
  * @see \App\Providers\Route::register
  */
 
-
 Route::portal('aamarpay', function () {
     Route::post('invoices/{invoice}/complete', 'Payment@complete')->name('invoices.complete');
     Route::post('invoices/{invoice}/return', 'Payment@return')->name('invoices.return');
-}, ['middleware' => ['web','auth.disabled',\Modules\Aamarpay\Http\Middleware\CompanyIdentifiy::class, 'bindings']]);
+}, ['middleware' => ['cookies.encrypt',
+'cookies.response',
+'session.start',
+'session.errors',
+'install.redirect',
+'header.x',
+'language',
+'firewall.all',
+'auth.disabled',
+\Modules\Aamarpay\Http\Middleware\CompanyIdentifiy::class, 
+'bindings']]);
